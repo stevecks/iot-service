5e87a00c548ec65227c816633ab10ce5c5dba7b4
Stepan <stepanzah71@gmail.com>
2024-07-22 12:18:38 +0300

    fix file conflict

--- src/Components/DevicesPage/DevicesPage.jsx
+++ src/Components/DevicesPage/DevicesPage.jsx
@@ -1,7 +1,17 @@
+import "./DevicesPage.css";
 import React from 'react';
+// import { Button } from '@mui/material';
+// import IconButton from '@mui/material/IconButton';
+//import TextField from '@mui/material/TextField';
+
 const DevicesPage = () => {
+
     return (
-        <div> DevicesPage </div>
+        <div className='container__back'>
+            <form className="form1">
+            <h2 className="form1__title">Сервис мониторинга для IoT устройств</h2>
+            </form>
+        </div>
     );
 };
 
